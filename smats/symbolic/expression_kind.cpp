3c8ddf6c12a01d117e5d0a5f07b11ce7cad0fccb
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-06-17 20:00:31 +0200

    feat: add expression kind

---
+++ smats/symbolic/expression_kind.cpp
@@ -0,0 +1,70 @@
+/**
+ * @author smats (https://github.com/TendTo/smats)
+ * @copyright 2024 smats
+ * @copyright 2019 Drake (https://drake.mit.edu)
+ * @licence Apache-2.0 license
+ */
+#include "smats/symbolic/expression_kind.h"
+
+namespace smats {
+
+std::ostream &operator<<(std::ostream &os, const ExpressionKind &kind) {
+  switch (kind) {
+    case ExpressionKind::Constant:
+      return os << "Constant";
+    case ExpressionKind::Var:
+      return os << "Var";
+    case ExpressionKind::Add:
+      return os << "Add";
+    case ExpressionKind::Mul:
+      return os << "Mul";
+    case ExpressionKind::Div:
+      return os << "Div";
+    case ExpressionKind::Pow:
+      return os << "Pow";
+    case ExpressionKind::Sin:
+      return os << "Sin";
+    case ExpressionKind::Cos:
+      return os << "Cos";
+    case ExpressionKind::Tan:
+      return os << "Tan";
+    case ExpressionKind::Asin:
+      return os << "Asin";
+    case ExpressionKind::Acos:
+      return os << "Acos";
+    case ExpressionKind::Atan:
+      return os << "Atan";
+    case ExpressionKind::Atan2:
+      return os << "Atan2";
+    case ExpressionKind::Sinh:
+      return os << "Sinh";
+    case ExpressionKind::Cosh:
+      return os << "Cosh";
+    case ExpressionKind::Tanh:
+      return os << "Tanh";
+    case ExpressionKind::Log:
+      return os << "Log";
+    case ExpressionKind::Exp:
+      return os << "Exp";
+    case ExpressionKind::Sqrt:
+      return os << "Sqrt";
+    case ExpressionKind::Abs:
+      return os << "Abs";
+    case ExpressionKind::Floor:
+      return os << "Floor";
+    case ExpressionKind::Ceil:
+      return os << "Ceil";
+    case ExpressionKind::Min:
+      return os << "Min";
+    case ExpressionKind::Max:
+      return os << "Max";
+    case ExpressionKind::IfThenElse:
+      return os << "IfThenElse";
+    case ExpressionKind::NaN:
+      return os << "NaN";
+    case ExpressionKind::UninterpretedFunction:
+      return os << "UninterpretedFunction";
+  }
+}
+
+}  // namespace smats
---
+++ smats/symbolic/expression_kind.h
@@ -0,0 +1,60 @@
+/**
+ * @author smats (https://github.com/TendTo/smats)
+ * @copyright 2024 smats
+ * @copyright 2019 Drake (https://drake.mit.edu)
+ * @licence Apache-2.0 license
+ * @brief ExpressionKind enum
+ *
+ * Kinds of symbolic expressions.
+ */
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+
+#include "smats/util/logging.h"
+
+namespace smats {
+
+/**
+ * Kinds of symbolic expressions.
+ */
+enum class ExpressionKind : std::uint16_t {
+  Constant,               ///< constant (double)
+  Var,                    ///< variable
+  Add,                    ///< addition (+)
+  Mul,                    ///< multiplication (*)
+  Div,                    ///< division (/)
+  Log,                    ///< logarithms
+  Abs,                    ///< absolute value function
+  Exp,                    ///< exponentiation
+  Sqrt,                   ///< square root
+  Pow,                    ///< power function
+  Sin,                    ///< sine
+  Cos,                    ///< cosine
+  Tan,                    ///< tangent
+  Asin,                   ///< arcsine
+  Acos,                   ///< arccosine
+  Atan,                   ///< arctangent
+  Atan2,                  ///< arctangent2 (atan2(y,x) = atan(y/x))
+  Sinh,                   ///< hyperbolic sine
+  Cosh,                   ///< hyperbolic cosine
+  Tanh,                   ///< hyperbolic tangent
+  Min,                    ///< min
+  Max,                    ///< max
+  Ceil,                   ///< ceil
+  Floor,                  ///< floor
+  IfThenElse,             ///< if then else
+  NaN,                    ///< NaN
+  UninterpretedFunction,  ///< Uninterpreted function
+};
+
+inline bool operator<(ExpressionKind k1, ExpressionKind k2) {
+  return static_cast<std::uint16_t>(k1) < static_cast<std::uint16_t>(k2);
+}
+
+std::ostream &operator<<(std::ostream &os, ExpressionKind kind);
+
+}  // namespace smats
+
+OSTREAM_FORMATTER(smats::ExpressionKind);
