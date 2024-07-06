b9af9596b3961979a91ff73ecc8fcaecfb29df0d
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-07-06 16:42:52 +0200

    feat: add symbolic module header

---
+++ smats/symbolic/symbolic.h
@@ -0,0 +1,14 @@
+/**
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024 smats
+ * @licence Apache-2.0 license
+ * Symbolic module
+ *
+ * This module contains the classes and functions to work with symbolic expressions and formulas.
+ */
+#pragma once
+
+#include "smats/symbolic/environment.h"
+#include "smats/symbolic/expression_kind.h"
+#include "smats/symbolic/variable.h"
+#include "smats/symbolic/variables.h"
